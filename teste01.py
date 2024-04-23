76c28a7905750adccb67dd1accb0fbdbfdaca77c
Rafael0703 <rafael.c.albino@gmail.com>
2024-04-23 16:37:33 -0300

    Criação variaveis.py

---
+++ teste01.py
@@ -0,0 +1,12 @@
+nome="Rafael Campos"
+idade=16
+email="rafaelcampos.gam@gmail.com"
+telefone="17 99656-7513"
+nota=9.00
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
