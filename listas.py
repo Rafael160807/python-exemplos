2789d995c67d1bec4acad869a27f0a3617609c25
Rafael0703 <rafael.c.albino@gmail.com>
2024-05-21 14:53:21 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["Rafael Campos" , 20 , 9 , True]
+print(f"Nome: {aluno[0]}")
+print(f"Faltas: {aluno[1]}")
+print(f"Media: {aluno[2]}")
+if aluno[3] == True:
+    print(f"Aprovado: Sim")
+else:
+    print(f"Aprovado: Não")
