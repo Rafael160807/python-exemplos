91c4dfb7dc7bf4d9b026821270ad41d9c5468c5f
Rafael0703 <rafael.c.albino@gmail.com>
2024-05-23 10:09:04 -0300

    criação e teste

--- lista_remover.py
+++ lista_remover.py
@@ -1,1 +1,11 @@
-alunos=['Adriana', 'João', 'Neto', 'Gabriel', 'Mateus', 'Natalia']
+alunos=['Adriana', 'João', 'Neto', 'Gabriel', 'Mateus', 'Natalia', 'Adriana']
+print(f"Lista de alunos: {alunos}")
+# Metodo Remove()
+alunos.remove('Adriana')
+print(f"Lista de alunos: {alunos}")
+# Metodo pop()
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+# Função del
+del alunos[3]
+print(f"Lista de alunos: {alunos}")
